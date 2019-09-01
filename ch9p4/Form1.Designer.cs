47009d922a0a3fd42eac0399cbb9805909563ed5
arick-mounts <54693943+arick-mounts@users.noreply.github.com>
2019-09-01 18:41:25 -0500

    Add project files.

---
+++ ch9p4/Form1.Designer.cs
@@ -0,0 +1,252 @@
+﻿namespace ch9p4
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.Row3_4 = new System.Windows.Forms.TextBox();
+            this.Row3_3 = new System.Windows.Forms.TextBox();
+            this.Row3_2 = new System.Windows.Forms.TextBox();
+            this.Row3_1 = new System.Windows.Forms.TextBox();
+            this.Row2_4 = new System.Windows.Forms.TextBox();
+            this.Row2_3 = new System.Windows.Forms.TextBox();
+            this.Row2_2 = new System.Windows.Forms.TextBox();
+            this.Row2_1 = new System.Windows.Forms.TextBox();
+            this.Row1_4 = new System.Windows.Forms.TextBox();
+            this.Row1_3 = new System.Windows.Forms.TextBox();
+            this.Row1_2 = new System.Windows.Forms.TextBox();
+            this.Row1_1 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.tableLayoutPanel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.ColumnCount = 4;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 53.1496F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 46.8504F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 137F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 155F));
+            this.tableLayoutPanel1.Controls.Add(this.Row3_4, 3, 3);
+            this.tableLayoutPanel1.Controls.Add(this.Row3_3, 2, 3);
+            this.tableLayoutPanel1.Controls.Add(this.Row3_2, 1, 3);
+            this.tableLayoutPanel1.Controls.Add(this.Row3_1, 0, 3);
+            this.tableLayoutPanel1.Controls.Add(this.Row2_4, 3, 2);
+            this.tableLayoutPanel1.Controls.Add(this.Row2_3, 2, 2);
+            this.tableLayoutPanel1.Controls.Add(this.Row2_2, 1, 2);
+            this.tableLayoutPanel1.Controls.Add(this.Row2_1, 0, 2);
+            this.tableLayoutPanel1.Controls.Add(this.Row1_4, 3, 1);
+            this.tableLayoutPanel1.Controls.Add(this.Row1_3, 2, 1);
+            this.tableLayoutPanel1.Controls.Add(this.Row1_2, 1, 1);
+            this.tableLayoutPanel1.Controls.Add(this.Row1_1, 0, 1);
+            this.tableLayoutPanel1.Controls.Add(this.textBox4, 3, 0);
+            this.tableLayoutPanel1.Controls.Add(this.textBox3, 2, 0);
+            this.tableLayoutPanel1.Controls.Add(this.textBox2, 1, 0);
+            this.tableLayoutPanel1.Controls.Add(this.textBox1, 0, 0);
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(73, 42);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 4;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 56.86274F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 43.13726F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 22F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 163F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(602, 238);
+            this.tableLayoutPanel1.TabIndex = 0;
+            this.tableLayoutPanel1.Paint += new System.Windows.Forms.PaintEventHandler(this.tableLayoutPanel1_Paint);
+            // 
+            // Row3_4
+            // 
+            this.Row3_4.Location = new System.Drawing.Point(449, 77);
+            this.Row3_4.Name = "Row3_4";
+            this.Row3_4.ReadOnly = true;
+            this.Row3_4.Size = new System.Drawing.Size(149, 20);
+            this.Row3_4.TabIndex = 15;
+            // 
+            // Row3_3
+            // 
+            this.Row3_3.Location = new System.Drawing.Point(312, 77);
+            this.Row3_3.Name = "Row3_3";
+            this.Row3_3.ReadOnly = true;
+            this.Row3_3.Size = new System.Drawing.Size(131, 20);
+            this.Row3_3.TabIndex = 14;
+            // 
+            // Row3_2
+            // 
+            this.Row3_2.Location = new System.Drawing.Point(167, 77);
+            this.Row3_2.Name = "Row3_2";
+            this.Row3_2.ReadOnly = true;
+            this.Row3_2.Size = new System.Drawing.Size(139, 20);
+            this.Row3_2.TabIndex = 13;
+            // 
+            // Row3_1
+            // 
+            this.Row3_1.Location = new System.Drawing.Point(3, 77);
+            this.Row3_1.Name = "Row3_1";
+            this.Row3_1.ReadOnly = true;
+            this.Row3_1.Size = new System.Drawing.Size(158, 20);
+            this.Row3_1.TabIndex = 12;
+            // 
+            // Row2_4
+            // 
+            this.Row2_4.Location = new System.Drawing.Point(449, 55);
+            this.Row2_4.Name = "Row2_4";
+            this.Row2_4.ReadOnly = true;
+            this.Row2_4.Size = new System.Drawing.Size(149, 20);
+            this.Row2_4.TabIndex = 11;
+            // 
+            // Row2_3
+            // 
+            this.Row2_3.Location = new System.Drawing.Point(312, 55);
+            this.Row2_3.Name = "Row2_3";
+            this.Row2_3.ReadOnly = true;
+            this.Row2_3.Size = new System.Drawing.Size(131, 20);
+            this.Row2_3.TabIndex = 10;
+            // 
+            // Row2_2
+            // 
+            this.Row2_2.Location = new System.Drawing.Point(167, 55);
+            this.Row2_2.Name = "Row2_2";
+            this.Row2_2.ReadOnly = true;
+            this.Row2_2.Size = new System.Drawing.Size(139, 20);
+            this.Row2_2.TabIndex = 9;
+            // 
+            // Row2_1
+            // 
+            this.Row2_1.Location = new System.Drawing.Point(3, 55);
+            this.Row2_1.Name = "Row2_1";
+            this.Row2_1.ReadOnly = true;
+            this.Row2_1.Size = new System.Drawing.Size(158, 20);
+            this.Row2_1.TabIndex = 8;
+            // 
+            // Row1_4
+            // 
+            this.Row1_4.Location = new System.Drawing.Point(449, 33);
+            this.Row1_4.Name = "Row1_4";
+            this.Row1_4.ReadOnly = true;
+            this.Row1_4.Size = new System.Drawing.Size(149, 20);
+            this.Row1_4.TabIndex = 7;
+            // 
+            // Row1_3
+            // 
+            this.Row1_3.Location = new System.Drawing.Point(312, 33);
+            this.Row1_3.Name = "Row1_3";
+            this.Row1_3.ReadOnly = true;
+            this.Row1_3.Size = new System.Drawing.Size(131, 20);
+            this.Row1_3.TabIndex = 6;
+            // 
+            // Row1_2
+            // 
+            this.Row1_2.Location = new System.Drawing.Point(167, 33);
+            this.Row1_2.Name = "Row1_2";
+            this.Row1_2.ReadOnly = true;
+            this.Row1_2.Size = new System.Drawing.Size(139, 20);
+            this.Row1_2.TabIndex = 5;
+            // 
+            // Row1_1
+            // 
+            this.Row1_1.Location = new System.Drawing.Point(3, 33);
+            this.Row1_1.Name = "Row1_1";
+            this.Row1_1.ReadOnly = true;
+            this.Row1_1.Size = new System.Drawing.Size(158, 20);
+            this.Row1_1.TabIndex = 4;
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(449, 3);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.ReadOnly = true;
+            this.textBox4.Size = new System.Drawing.Size(149, 20);
+            this.textBox4.TabIndex = 3;
+            this.textBox4.Text = "Position";
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(312, 3);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.ReadOnly = true;
+            this.textBox3.Size = new System.Drawing.Size(131, 20);
+            this.textBox3.TabIndex = 2;
+            this.textBox3.Text = "Department";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(167, 3);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.ReadOnly = true;
+            this.textBox2.Size = new System.Drawing.Size(139, 20);
+            this.textBox2.TabIndex = 1;
+            this.textBox2.Text = "ID Number";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(3, 3);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ReadOnly = true;
+            this.textBox1.Size = new System.Drawing.Size(158, 20);
+            this.textBox1.TabIndex = 0;
+            this.textBox1.Text = "Name";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(731, 288);
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.tableLayoutPanel1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox Row3_4;
+        private System.Windows.Forms.TextBox Row3_3;
+        private System.Windows.Forms.TextBox Row3_2;
+        private System.Windows.Forms.TextBox Row3_1;
+        private System.Windows.Forms.TextBox Row2_4;
+        private System.Windows.Forms.TextBox Row2_3;
+        private System.Windows.Forms.TextBox Row2_2;
+        private System.Windows.Forms.TextBox Row2_1;
+        private System.Windows.Forms.TextBox Row1_4;
+        private System.Windows.Forms.TextBox Row1_3;
+        private System.Windows.Forms.TextBox Row1_2;
+        private System.Windows.Forms.TextBox Row1_1;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox2;
+    }
+}
+
---
+++ ch9p4/Form1.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ch9p4
+{
+
+
+    public partial class Form1 : Form
+    {
+
+        /*
+         * This program constructs several employee classes and displays them in a table
+         * C# @TCC
+         * 
+         * Arick Mounts
+         */
+        public Form1()
+        {
+            InitializeComponent();
+            Employee Employee1 = new Employee("Susan Meyers", 47899,"Accounting", "Vice President");
+            Employee Employee2 = new Employee("Mark Jones", 39119, "IT", "Programmer");
+            Employee Employee3 = new Employee("Joy Rogers", 81774, "Manufacturing", "Engineer");
+            Row1_1.Text = Employee1.Name;
+            Row1_2.Text = Employee1.IdNumber.ToString();
+            Row1_3.Text = Employee1.Department;
+            Row1_4.Text = Employee1.Position;
+
+            Row2_1.Text = Employee2.Name;
+            Row2_2.Text = Employee2.IdNumber.ToString();
+            Row2_3.Text = Employee2.Department;
+            Row2_4.Text = Employee2.Position;
+
+            Row3_1.Text = Employee3.Name;
+            Row3_2.Text = Employee3.IdNumber.ToString();
+            Row3_3.Text = Employee3.Department;
+            Row3_4.Text = Employee3.Position;
+        }
+
+        private void tableLayoutPanel1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+    }
+    class Employee
+    {
+        private string _Name;
+        private int _IdNumber;
+        private string _Position;
+        private string _Department;
+
+        //constructor
+        public Employee()
+        {
+            _Name = "";
+            _IdNumber = 0;
+            _Position = "";
+            _Department = "";
+        }
+
+        public Employee(string N, int I)
+        {
+            _Name = N;
+            _IdNumber = I;
+            _Position = "";
+            _Department = "";
+        }
+
+        public Employee(string N, int I, string P, string D)
+        {
+            _Name = N;
+            _IdNumber = I;
+            _Position = P;
+            _Department = D;
+        }
+        public string Name
+        {
+            get
+            {
+                return _Name;
+            }
+
+        }
+        public int IdNumber
+        {
+            get
+            {
+                return _IdNumber;
+            }
+        }
+        public string Position
+        {
+            get
+            {
+                return _Position;
+            }
+        }
+
+        public string Department
+        {
+            get
+            {
+                return _Department;
+            }
+        }
+
+    }
+}
